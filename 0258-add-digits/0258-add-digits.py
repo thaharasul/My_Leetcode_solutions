0580f2ebc41265fb29ea3f5ed5b5d931e2ded775
thaharasul <thaharasxl@gmail.com>
2026-08-27 12:02:57 +0530

    Time: 0 ms (100%), Space: 19.2 MB (54.28%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+           if num<10:
+                return num
+           while num>9:
+               num=num%10 + num//10
+           return num
